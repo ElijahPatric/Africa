cf53a2f8ad962a052d128a3ded067193a537bae2
Elijah Patric <foreverelijah@gmail.com>
2022-10-31 16:23:11 -0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Elijah on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
